61c5de682d1e100b24f1e8cd14c97c1a6b7a2d95
Kabilan Jayabalan <kabilan2k06@gmail.com>
2026-03-10 13:46:12 +0000

    First Commit

---
+++ Easy/242_Valid_Anagram.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int freq[]=new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i:s.toCharArray()){
+            freq[i-'a']++;
+        }
+        for(int i:t.toCharArray()){
+            freq[i-'a']--;
+        }
+        for(int i:freq){
+            if(i>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+Memory : 44.64 MB Beats 59.52%
+Runtime : 4 ms Beats 81.33%
+*/
